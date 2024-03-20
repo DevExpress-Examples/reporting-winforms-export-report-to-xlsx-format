d2d59b0208e392f3e1c36a9b416d7be86dfb5f95
DevExpressExampleBot <fakeEmail>
2024-03-20 14:55:13 +0400

    Source auto update [skip ci]

--- VB/ExportToXLSX/Form1.vb
+++ VB/ExportToXLSX/Form1.vb
@@ -1,51 +1,46 @@
-﻿Imports System
+Imports System
 Imports System.Diagnostics
 Imports System.Windows.Forms
 Imports DevExpress.XtraPrinting
+
 ' ...
-
 Namespace ExportToXLSX
-	Partial Public Class Form1
-		Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
-			' A path to export a report.
-			Dim reportPath As String = "c:\Test.xlsx"
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-			' Create a report instance.
-			Dim report As New XtraReport1()
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            ' A path to export a report.
+            Dim reportPath As String = "c:\Test.xlsx"
+            ' Create a report instance.
+            Dim report As XtraReport1 = New XtraReport1()
+            ' Get its XLS export options.
+            Dim xlsxOptions As XlsxExportOptions = report.ExportOptions.Xlsx
+            ' Set XLS-specific export options.
+            xlsxOptions.ShowGridLines = True
+            xlsxOptions.TextExportMode = TextExportMode.Value
+            xlsxOptions.ExportHyperlinks = True
+            xlsxOptions.SheetName = "My Sheet"
+            ' Export the report to XLS.
+            report.ExportToXlsx(reportPath)
+            ' Show the result.
+            StartProcess(reportPath)
+        End Sub
 
-			' Get its XLS export options.
-			Dim xlsxOptions As XlsxExportOptions = report.ExportOptions.Xlsx
-
-			' Set XLS-specific export options.
-			xlsxOptions.ShowGridLines = True
-			xlsxOptions.TextExportMode = TextExportMode.Value
-			xlsxOptions.ExportHyperlinks = True
-			xlsxOptions.SheetName = "My Sheet"
-
-			' Export the report to XLS.
-			report.ExportToXlsx(reportPath)
-
-			' Show the result.
-			StartProcess(reportPath)
-		End Sub
-
-		' Use this method if you want to automaically open
-		' the created XLS file in the default program.
-		Public Sub StartProcess(ByVal path As String)
-			Dim process As New Process()
-			Try
-				process.StartInfo.FileName = path
-				process.Start()
-				process.WaitForInputIdle()
-			Catch
-			End Try
-		End Sub
-
-	End Class
+        ' Use this method if you want to automaically open
+        ' the created XLS file in the default program.
+        Public Sub StartProcess(ByVal path As String)
+            Dim process As Process = New Process()
+            Try
+                process.StartInfo.FileName = path
+                process.Start()
+                process.WaitForInputIdle()
+            Catch
+            End Try
+        End Sub
+    End Class
 End Namespace
