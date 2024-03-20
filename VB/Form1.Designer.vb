207119541f59cdf391a79e14a35299752bf59001
DevExpressExampleBot <fakeEmail>
2024-03-20 17:24:39 +0400

    Source auto update [skip ci]

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,54 +1,54 @@
-﻿Namespace XlsxExportExample
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace XlsxExportExample
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.SimpleButton1 = New DevExpress.XtraEditors.SimpleButton()
-			Me.SuspendLayout()
-			'
-			'SimpleButton1
-			'
-			Me.SimpleButton1.Location = New System.Drawing.Point(65, 42)
-			Me.SimpleButton1.Name = "SimpleButton1"
-			Me.SimpleButton1.Size = New System.Drawing.Size(207, 46)
-			Me.SimpleButton1.TabIndex = 0
-			Me.SimpleButton1.Text = "Create a Report and Export to XLSX"
-			'
-			'Form1
-			'
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(336, 130)
-			Me.Controls.Add(Me.SimpleButton1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		Friend WithEvents SimpleButton1 As DevExpress.XtraEditors.SimpleButton
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.simpleButton1 = New DevExpress.XtraEditors.SimpleButton()
+            Me.SuspendLayout()
+            ' 
+            ' simpleButton1
+            ' 
+            Me.simpleButton1.Location = New System.Drawing.Point(65, 42)
+            Me.simpleButton1.Name = "simpleButton1"
+            Me.simpleButton1.Size = New System.Drawing.Size(207, 46)
+            Me.simpleButton1.TabIndex = 0
+            Me.simpleButton1.Text = "Create a Report and Export to XLSX"
+            AddHandler Me.simpleButton1.Click, New System.EventHandler(AddressOf Me.simpleButton1_Click)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(336, 130)
+            Me.Controls.Add(Me.simpleButton1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            Me.ResumeLayout(False)
+        End Sub
 
 #End Region
-	End Class
+        Private simpleButton1 As DevExpress.XtraEditors.SimpleButton
+    End Class
 End Namespace
-
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,38 +1,27 @@
-﻿Imports DevExpress.XtraPrinting
+Imports DevExpress.XtraPrinting
 Imports DevExpress.XtraReports.UI
 Imports System.Windows.Forms
 Imports System
 
 Namespace XlsxExportExample
-	Partial Public Class Form1
-		Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Public Partial Class Form1
+        Inherits Form
 
-		Private Sub SimpleButton1_Click(sender As Object, e As EventArgs) Handles SimpleButton1.Click
-			' Create a report.
-			Dim label As New XRLabel() With {.Text = "Some content goes here..."}
-			Dim band As New DetailBand()
-			band.Controls.Add(label)
-			Dim report As New XtraReport() With {.Name = "Report Example"}
-			report.Bands.Add(band)
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-
-			' Specify export options.
-			Dim xlsxExportOptions As New XlsxExportOptions() With {
-				.ExportMode = XlsxExportMode.SingleFile,
-				.ShowGridLines = True,
-				.FitToPrintedPageHeight = True
-			}
-
-			' Specify the path for the exported XLSX file.  
-			Dim xlsxExportFileName As String = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) & "\Downloads\" & report.Name & ".xlsx"
-
-			' Export the report.
-			report.ExportToXlsx(xlsxExportFileName, xlsxExportOptions)
-			System.Diagnostics.Process.Start(xlsxExportFileName)
-		End Sub
-	End Class
+        Private Sub simpleButton1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            ' Create a report.
+            Dim report As XtraReport = New XtraReport() With {.Name = "Report Example"}
+            ' Specify export options.
+            Dim xlsxExportOptions As XlsxExportOptions = New XlsxExportOptions() With {.ExportMode = XlsxExportMode.SingleFile, .ShowGridLines = True, .FitToPrintedPageHeight = True}
+            ' Specify the path for the exported XLSX file.  
+            Dim xlsxExportFileName As String = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) & "\Downloads\" & report.Name & ".xlsx"
+            ' Export the report.
+            report.ExportToXlsx(xlsxExportFileName, xlsxExportOptions)
+            System.Diagnostics.Process.Start(xlsxExportFileName)
+        End Sub
+    End Class
 End Namespace
